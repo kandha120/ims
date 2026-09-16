09fd4010d3db1d476db89418fea508fa1618a96d
prawin03sm <110221041+prawin03sm@users.noreply.github.com>
2026-09-16 15:52:44 +0530

    Modify ESLint config to ignore additional directories
    Updated ESLint configuration to exclude vendor assets and built output from linting.

--- IMS_FRONTEND_CURRENT/eslint.config.js
+++ IMS_FRONTEND_CURRENT/eslint.config.js
@@ -5,7 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { // Exclude vendor assets and built output from linting
+    ignores: ["src/assets/**", "dist/**", "build/**", "node_modules/**"] 
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
